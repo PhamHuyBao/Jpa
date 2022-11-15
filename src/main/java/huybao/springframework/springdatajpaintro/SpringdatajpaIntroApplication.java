19f40a9bdc96856aa31b6cfe793122fda654ce6b
Pham Bao <phambaose@gmail.com>
2022-11-15 12:19:04 +0700

    init commit

---
+++ src/main/java/huybao/springframework/springdatajpaintro/SpringdatajpaIntroApplication.java
@@ -0,0 +1,13 @@
+package huybao.springframework.springdatajpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/huybao/springframework/springdatajpaintro/SpringdatajpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package huybao.springframework.springdatajpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
